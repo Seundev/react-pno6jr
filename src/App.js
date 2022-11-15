7ba5780e763fc36c9771bf6dec663a25d3984ff5
Seundev <oyedeleoluwaseun56@gmail.com>
2022-11-15 23:07:49 +0100

    1 file modified, 1 deleted

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CustomHookPage from './components/CustomHookPage';
 import UseReducerPage from './components/UseReducerPage';
-import ErrorBoundary from './components/ErrorBoundary';
 import Home from './components/Home';
 import ErrorPage from './components/ErrorPage';
 import './style.css';
@@ -15,7 +14,6 @@ function App() {
           <Route path="/" element={<Home />} />;
           <Route path="/customhookpage" element={<CustomHookPage />} />;
           <Route path="/usereducerpage" element={<UseReducerPage />} />;
-          <Route path="/errorboundary" element={<ErrorBoundary />} />;
           <Route path="/errorpage" element={<ErrorPage />} />;
         </Routes>
       </Router>
